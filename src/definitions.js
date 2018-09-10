326e4c0333eb4847fec4d28bf64f752fbdba6b0e
Zach Posten <zach@posten.io>
2018-09-10 08:45:02 -0500

    chore: Add real word definitions

--- src/definitions.js
+++ src/definitions.js
@@ -1,32 +1,147 @@
 let definitions = {
-    'Buttonhole': [
-        "Buttonhole - def1",
-        "Buttonhole - def2",
-        "Buttonhole - def3",
+    'founder': [
+        'to make or become disabled or lame',
+        'to give away; collapse',
+        'to become submerged, sink',
+        'to come to grief, fail',
     ],
-    'Eopch': [
-        'Eopch - def1',
-        'Eopch - def2',
-        'Eopch - def3',
+    'winsome': [
+        'generally pleasing and engaging often because of a childlike charm and innoecence',
+        'cheerful, lighthearted',
     ],
-    'Nostrum': [
-        'Nostrum - def1',
+    'kafkaesque': [
+        'marked by a senseless, disorienting, often menacing complexity',
     ],
-    'Essence': [
-        'Essence - def1',
-        'Essence - def2',
-        'Essence - def3',
+    'callipygian': [
+        'having shapely buttocks',
     ],
-    'Kierstyn': [
-        'Kierstyn - def1',
-        'Kierstyn - def2',
-        'Kierstyn - def3',
-        'Kierstyn - def4',
-        'Kierstyn - def5',
+    'kismet': [
+        'destiny, fate',
     ],
-    'Zach': [
-        'Zach - def1',
-        'Zach - def2',
+    'refulgent': [
+        'giving off or reflecting much light',
+    ],
+    'physiognomy': [
+        'the human face',
+        'the method of learning about a person from studying the face',
+    ],
+    'duende': [
+        'the power to attract through personal magnitism and charm',
+    ],
+    'esoteric': [
+        'requiring or exhibiting knowledge that is restricted to a small group',
+        'difficult to understand',
+    ],
+    'temerity': [
+        'unreasonable or foolhardy contempt of danger or opposition: rashness, recklessness',
+        'a rash or reclkess act',
+    ],
+    'tristful': [
+        'sad, melancholy',
+    ],
+    'tocsin': [
+        'an alarm bell or the ringing of it',
+        'a warning signal',
+    ],
+    'rapture': [
+        'a feeling of intense pleasure or joy',
+    ],
+    'convivial': [
+        '(of an atmosphere or event) friendly, lively and enjoyable',
+        '(of a person) cheerful and friendly; jovial',
+    ],
+    'adombrate': [
+        'to foreshadow vaguely',
+        'to suggest, disclose, or outline partially',
+    ],
+    'mantic': [
+        'of or relating to the faculty of divination, prophetic',
+    ],
+    'defenestration': [
+        'throwing of a person out a window',
+        'a swift dismissal or expulsion',
+    ],
+    'logomachy': [
+        'a dispute over or about words',
+        'a controversy marked by verbiage',
+    ],
+    'sensationalism': [
+        'the use of exciting or shocking stories or language at the expense of accuracy, in order to provoke public interest or excitement',
+    ],
+    'languor': [
+        'the state or feeling, often plesent, of tiredness or inertia',
+        'an oppressive stillness of the air',
+    ],
+    'transmogrify': [
+        'to change or alter greatly and often with grotesque or humorous effect',
+    ],
+    'yawp': [
+        'to amke a rauccous noise; squawk',
+        'clamour, complain',
+    ],
+    'mercurial': [
+        'of, orlating to, or born under the planet mercury',
+        'having qualities of eloquence, ingenuity, or thievishness to the god of Mercury',
+        'characterized by reapid and unpredictable chanableness of mood',
+    ],
+    'woolgathering': [
+        'indulgence in idle daydreaming',
+    ],
+    'agon': [
+        'conflict, especially the dramatic conflict between chief characters in a literary work',
+    ],
+    'muliebrity': [
+        'the quality of being a woman; femininity',
+    ],
+    'mellifluous': [
+        'having a smooch, rich flow',
+    ],
+    'confabulate': [
+        'to converse informally; chat',
+    ],
+    'acquiesce': [
+        'to accept, comply, or submit tacitly or passively',
+    ],
+    'vacillate': [
+        'to waver in mind, will, or feeling: hesitant in choice of opinion or courses',
+    ],
+    'tempestuous': [
+        'relating to a violent storm',
+    ],
+    'gaffer': [
+        'an old man',
+        'British: foreman, overseer, employer',
+        'a head glassblower',
+        'a lighting electrician on a movie or television recording set',
+    ],
+    'epoch': [
+        'an event or a time that marks a new period of development',
+    ],
+    'exigent': [
+        'requiring immediate air or action',
+        'requiring or calling for much; demaning',
+    ],
+    'purfle': [
+        'to ornament the border or edges of',
+    ],
+    'abscond': [
+        'to deparpt secretly and hide oneself',
+    ],
+    'prevaricate': [
+        'to deviate form the truth; equivocate',
+    ],
+    'buttonhole': [
+        'to detain in conversation by or as if by holding on to the outer garments of',
+    ],
+    'fruition': [
+        'pleasurable use or possession; enjoyment',
+        'the state of bearing fruit',
+        'realization',
+    ],
+    'quiddity': [
+        'whatever makes something the type that it is; essence',
+        'a trifling point; quibble',
+        'an unusual personal opinion or habit; eccentricity',
     ],
 }
 
